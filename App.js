b678cde63ee9d71677c112777f804a981e1d8803
axelvestberg <axel.vestberg@gmail.com>
2018-03-01 16:21:27 +0100

    changed welcome text

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Välkommen till react native!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
